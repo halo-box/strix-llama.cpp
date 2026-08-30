Nathanw1014/llama.cpp

vulkan: DeepSeek V4 lightning indexer kernels + indexed sparse FA
Implements GGML_OP_LIGHTNING_INDEXER on Vulkan (scalar subgroup shader for
small batches, coopmat 16x16 tiles for prefill, dedicated decode variant)
and an indexed sparse flash-attention path that consumes the indexer's
top-k selection directly via a new ggml_flash_attn_ext_add_top_k() API
(FA src[5] + op_param[4] = n_kv_raw dense prefix), instead of attending
densely over the full compressed KV.

The sparse path engages only for V4's CSA shape (hd 512, 64 heads, MQA,
f16 K==V latent) when dense_kv >= 3x active_kv; everything else falls
through to the dense path, which stays correct because the kq_mask still
carries the top-k selection.

Dropped from the original: the mul_mat_id tokens-per-expert pipeline
selection, which duplicates GGML_VK_MMID_SMALLN already on this branch.

Originally by Gaetan Puleo (llama-cpp-nathan-toolbox-deepseek-v4-poc,
branch deepseek-v4-flash-strix-halo); cherry-picked with the mmid hunk
dropped.

ggml/src/ggml-vulkan/vulkan-shaders/vulkan-shaders-gen.cpp
@@ -800,6 +800,13 @@ void process_shaders() {
 
     string_to_spv("get_rows_i32", "get_rows.comp", {{"TEMP_TYPE", "uint"}, {"A_TYPE", "uint"}, {"B_TYPE", "int"}, {"D_TYPE", "uint"}});
 
+    string_to_spv("lightning_indexer_f16", "lightning_indexer.comp", {});
+#if defined(GGML_VULKAN_COOPMAT_GLSLC_SUPPORT)
+    string_to_spv("lightning_indexer_cm_f16", "lightning_indexer_cm.comp", {});
+    string_to_spv("lightning_indexer_decode_cm_f16", "lightning_indexer_decode_cm.comp", {});
+#endif
+    string_to_spv("flash_attn_top_k_f16", "flash_attn_top_k.comp", {});
+
     string_to_spv("mul_mat_vec_p021_f16_f32_subgroup_add", "mul_mat_vec_p021.comp", {{"A_TYPE", "float16_t"}, {"A_TYPEV4", "f16vec4"}, {"B_TYPE", "float"}, {"B_TYPEV4", "vec4"}, {"D_TYPE", "float"}, {"USE_SUBGROUP_ADD", "1"}});
     string_to_spv("mul_mat_vec_p021_f16_f32",              "mul_mat_vec_p021.comp", {{"A_TYPE", "float16_t"}, {"A_TYPEV4", "f16vec4"}, {"B_TYPE", "float"}, {"B_TYPEV4", "vec4"}, {"D_TYPE", "float"}});
     string_to_spv("mul_mat_vec_nc_f16_f32", "mul_mat_vec_nc.comp", {{"A_TYPE", "float16_t"}, {"A_TYPEV4", "f16vec4"}, {"B_TYPE", "float"}, {"B_TYPEV4", "vec4"}, {"D_TYPE", "float"}});

halo-box/strix-llama.cpp

ggml/src/ggml-vulkan/vulkan-shaders/vulkan-shaders-gen.cpp
@@ -801,6 +801,13 @@ void process_shaders() {
 
     string_to_spv("get_rows_i32", "get_rows.comp", {{"TEMP_TYPE", "uint"}, {"A_TYPE", "uint"}, {"B_TYPE", "int"}, {"D_TYPE", "uint"}});
 
+    string_to_spv("lightning_indexer_f16", "lightning_indexer_scalar64.comp", {});
+#if defined(GGML_VULKAN_COOPMAT_GLSLC_SUPPORT)
+    string_to_spv("lightning_indexer_cm_f16", "lightning_indexer_cm.comp", {});
+    string_to_spv("lightning_indexer_decode_cm_f16", "lightning_indexer_decode_cm.comp", {});
+#endif
+    string_to_spv("flash_attn_top_k_f16", "flash_attn_top_k.comp", {});
+
     string_to_spv("mul_mat_vec_p021_f16_f32_subgroup_add", "mul_mat_vec_p021.comp", {{"A_TYPE", "float16_t"}, {"A_TYPEV4", "f16vec4"}, {"B_TYPE", "float"}, {"B_TYPEV4", "vec4"}, {"D_TYPE", "float"}, {"USE_SUBGROUP_ADD", "1"}});
     string_to_spv("mul_mat_vec_p021_f16_f32",              "mul_mat_vec_p021.comp", {{"A_TYPE", "float16_t"}, {"A_TYPEV4", "f16vec4"}, {"B_TYPE", "float"}, {"B_TYPEV4", "vec4"}, {"D_TYPE", "float"}});
     string_to_spv("mul_mat_vec_nc_f16_f32", "mul_mat_vec_nc.comp", {{"A_TYPE", "float16_t"}, {"A_TYPEV4", "f16vec4"}, {"B_TYPE", "float"}, {"B_TYPEV4", "vec4"}, {"D_TYPE", "float"}});